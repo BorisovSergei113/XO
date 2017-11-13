b21fa52773585d638caa5908f82b40678f1566e6
BorisovSergei113 <32065890+BorisovSergei113@users.noreply.github.com>
2017-11-13 20:45:01 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 
 char ** init_pole(char **pole){
- pole = new char * [3];
+    pole = new char * [3];
     for (int i = 0 ; i<3; i++) {
-            pole[i]=new char [3];
+        pole[i]=new char [3];
         for (int j =0; j<3; j++) {
             pole[i][j]='0';
         }
@@ -17,15 +17,15 @@ char ** init_pole(char **pole){
 void next_turn(char ** pole,bool player){
     int choose =1;
     if (player){
-    for (int i =0; i<3; i++) {
-        for(int j =0;j<3;j++){
-            if(pole[i][j]=='0'){
-                cout<<choose<<"->"<<char(i+97)<<char(j+49)<<"(Player X)"<<endl;
-                choose++;
+        for (int i =0; i<3; i++) {
+            for(int j =0;j<3;j++){
+                if(pole[i][j]=='0'){
+                    cout<<choose<<"->"<<char(i+97)<<char(j+49)<<"(Player X)"<<endl;
+                    choose++;
+                }
             }
         }
     }
-}
     if (!player){
         for (int i =0; i<3; i++) {
             for(int j =0;j<3;j++){
@@ -38,6 +38,8 @@ void next_turn(char ** pole,bool player){
     }
     cout<<choose<<"->quit"<<endl;
 }
+
+void del_mem(char **);
 
 void next_turn(char **pole,int turn,bool player){
     int choose =1;
@@ -58,6 +60,7 @@ void next_turn(char **pole,int turn,bool player){
     }
     if(turn==choose){
         cout<<"Quit"<<endl;
+        del_mem(pole);
         exit(0);
     }
 }
@@ -71,22 +74,22 @@ int case_chose(){
 bool proverka_hor(char **pole){
     for (int i =0; i<3; i++) {
         if(pole[i][0]=='X' && pole[i][1]=='X' && pole[i][2]=='X'){
-        cout<<"Player"<<"  "<<pole[i][0]<<" win"<<endl;
-         return true;
+            cout<<"Player"<<"  "<<pole[i][0]<<" win"<<endl;
+            return true;
         }
         if(pole[i][0]=='O' && pole[i][1]=='O' && pole[i][2]=='O'){
-             cout<<"Player"<<"  "<<pole[i][0]<<" win"<<endl;
+            cout<<"Player"<<"  "<<pole[i][0]<<" win"<<endl;
             return true;
+        }
     }
-    }
-        return false;
+    return false;
 }
 
 bool proverka_ver(char **pole){
     for (int j=0; j<3; j++) {
         if(pole[0][j]=='X' && pole[1][j]=='X' && pole[2][j]=='X'){
-          cout<<"Player"<<"  "<<pole[0][j]<<" win"<<endl;
-        return true;
+            cout<<"Player"<<"  "<<pole[0][j]<<" win"<<endl;
+            return true;
         }
         if(pole[0][j]=='O' && pole[1][j]=='O' && pole[2][j]=='O'){
             cout<<"Player"<<"  "<<pole[0][j]<<" win"<<endl;
@@ -98,16 +101,16 @@ bool proverka_ver(char **pole){
 
 bool proverka_d(char **pole){
     if(pole[0][0]=='X' && pole[1][1]=='X' && pole[2][2]=='X'){
-     cout<<"Player"<<"  "<<pole[0][0]<<" win"<<endl;
-    return true;
+        cout<<"Player"<<"  "<<pole[0][0]<<" win"<<endl;
+        return true;
     }
     if(pole[0][0]=='O' && pole[1][1]=='O' && pole[2][2]=='O'){
-         cout<<"Player"<<"  "<<pole[0][0]<<" win"<<endl;
+        cout<<"Player"<<"  "<<pole[0][0]<<" win"<<endl;
         return true;
     }
     if(pole[0][2]=='X' && pole[1][1]=='X' && pole[2][0]=='X'){
-      cout<<"Player"<<"  "<<pole[0][2]<<" win"<<endl;
-    return true;
+        cout<<"Player"<<"  "<<pole[0][2]<<" win"<<endl;
+        return true;
     }
     if(pole[0][2]=='O' && pole[1][1]=='O' && pole[2][0]=='O'){
         cout<<"Player"<<"  "<<pole[0][2]<<" win"<<endl;
@@ -151,7 +154,7 @@ int main() {
     char **pole = nullptr;
     bool player = true;
     int xod=0;
-    
+
     pole=init_pole(pole);
     out_pole(pole);
     while (!win_game(pole) && xod!=9) {
@@ -162,8 +165,8 @@ int main() {
         player=!player;
     }
     if(xod==9) cout<<"Draw"<<endl;
-    
+
     del_mem(pole);
-    
+
     return 0;
 }
